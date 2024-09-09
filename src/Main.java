f16923216eacc25c6cfda9d4d4a433fa71215fd8
borntofall <vsexobnyal@yandex.ru>
2024-09-10 04:33:13 +0600

    hw done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.*;
+
 public class Main {
+
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+
+    public static void task1() {
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+    }
+
+    public static void task2() {
+        Collections.sort(nums);
+
+        int uniNum = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num % 2 != 0 && num != uniNum) ;
+            System.out.println(num);
+            uniNum = num;
+        }
+    }
+
+
+    public static void task3() {
+        Set<String> uniqueStrings = new HashSet<>(strings);
+        System.out.println(uniqueStrings);
+    }
+
+    public static void task4() {
+        Set<String> uniqueStrings = new HashSet<>(strings);
+        System.out.println(strings.size() - uniqueStrings.size());
     }
 }
